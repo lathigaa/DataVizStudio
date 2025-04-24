0e29d235a80c68059168fde2713139a7c7480b30
lathigaa <119679268+lathigaa@users.noreply.github.com>
2025-04-24 10:52:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -149,7 +149,11 @@ def generate_chart():
             'error': str(e)
         })
     
+
+
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 10000))  # Default to 10000 if not provided
+    app.run(host='0.0.0.0', port=port)
     app.run(debug=True)
 
     
